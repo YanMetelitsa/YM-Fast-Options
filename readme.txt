53ed1d6fb0a12f96f0b960951144536afa8a831a
Yan Metelitsa <yan.metelitsa@gmail.com>
2024-11-16 03:45:59 +0300

    WP 6.7

--- readme.txt
+++ readme.txt
@@ -4,7 +4,7 @@ Tags: options, settings
 Stable tag: 2.1.0
 Requires PHP: 7.4
 Requires at least: 6.0
-Tested up to: 6.6.2
+Tested up to: 6.7
 License: GPLv3
 License URI: https://www.gnu.org/licenses/gpl-3.0.html
 
--- ym-fast-options.php
+++ ym-fast-options.php
@@ -6,7 +6,7 @@
  * Version:           2.1.0
  * Requires PHP:      7.4
  * Requires at least: 6.0
- * Tested up to:      6.6.2
+ * Tested up to:      6.7
  * Author:            Yan Metelitsa
  * Author URI:        https://yanmet.com/
  * License:           GPLv3
